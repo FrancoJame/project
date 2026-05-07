f304c64c21e2e0871e96c0f6af6dce1ccde0a71c
FrancoJame <mutebifrancis33@gmail.com>
2026-05-08 01:50:48 +0300

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,3 +5,15 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ('email', 'phone')
+
+class StaffCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('email', 'phone')
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.role = 'STAFF'
+        if commit:
+            user.save()
+        return user
